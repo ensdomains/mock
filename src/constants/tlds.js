c6f6f0c8b578241e12a6453eabc093eb4ece3cb5
Jeff Lau <jeff-lau@live.com>
2019-05-30 11:54:15 +0800

    Initial commit

---
+++ src/constants/tlds.js
@@ -0,0 +1,17 @@
+export const tlds = {
+  eth: {
+    supported: true
+  },
+  xyz: {
+    supported: true
+  },
+  test: {
+    supported: true
+  },
+  reverse: {
+    supported: true
+  },
+  luxe: {
+    supported: true
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import deployENS from './testing-utils/deployENS'
+import { mockENS } from './testing-utils/preTest'
+
+export { deployENS, mockENS }
